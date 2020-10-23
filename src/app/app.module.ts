f740a1581674423053675746d4ab5f7a00323d8b
Miguel <sorth29@gmail.com>
2020-10-23 14:23:22 -0500

    primer fas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+
+const ruta: Routes = [
+  {
+    path: 'inicio',
+    component: ProyectosComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProyectosComponent,
+    FooterComponent,
+    ContenidoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ruta,
+      { useHash: true }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
